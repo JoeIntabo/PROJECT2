17cb3a50a426ad836222b239f799855d17ad83b8
JoeIntabo <intabojoe@gmail.com>
2023-07-14 05:50:07 +0300

    initial commmit

--- app/src/main/java/com/example/project2/Menucard.kt
+++ app/src/main/java/com/example/project2/Menucard.kt
@@ -3,10 +3,14 @@ package com.example.project2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
 import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
 //import com.example.project2.databinding.ActivityMenucardBinding
 //import com.google.firebase.database.DatabaseReference
 //import com.google.firebase.database.FirebaseDatabase
@@ -18,7 +22,7 @@ class Menucard : AppCompatActivity() {
     private lateinit var orderAdapter: ArrayAdapter<String>
     private lateinit var placeOrderButton: Button
     // private lateinit var binding:ActivityMenucardBinding
-    //private lateinit var database: DatabaseReference
+    //private lateinit var database: DatabaseReferenceval itemData = getItemData(selectedItem)
 
     private val menuItems = arrayOf(
         "TEA KSH.50",
@@ -32,61 +36,91 @@ class Menucard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menucard)
-        // binding=ActivityMenucardBinding.inflate(layoutInflater)
-        //    setContentView(binding.root)
 
-        //  binding.placeOrderButton.setOnClickListener {
-        //     val order= listOf(binding.orderListView).toString()
-
-        //    database=FirebaseDatabase.getInstance().getReference("Order")
-        //   val orderlist=database(order)
-        //      database.child(order).setValue(orderlist).addOnSuccessListener {
-        //       listOf(binding.orderListView).toString()
-        //        Toast.makeText(this,"Successfully Saved",Toast.LENGTH_SHORT).show()
-        //    }.addOnFailureListener {
-        //             Toast.makeText(this,"Failed to Save",Toast.LENGTH_SHORT).show()
+        // Get the reference to the Firebase Realtime Database
+        val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+        val databaseRef: DatabaseReference = database.reference
 
 
-        menuListView = findViewById(R.id.menuListView)
-        orderListView = findViewById(R.id.orderListView)
-        placeOrderButton = findViewById(R.id.placeOrderButton)
+        // Assuming you have a ListView with id "listView" in your XML layout
+        val listView: ListView = findViewById(R.id.menuListView)
 
-        // Set up menu ListView
-        val menuAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, menuItems)
-        menuListView.adapter = menuAdapter
+// Assuming you have a Button with id "pushButton" in your XML layout
+        val placeOrderB: Button = findViewById(R.id.placeOrderButton)
 
-        // Set up order ListView
-        orderAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, selectedItems)
-        orderListView.adapter = orderAdapter
+        var selectedItem: Menucard.Orderlist? = null
 
-        // Add item to order when clicked
-        menuListView.setOnItemClickListener { _, _, position, _ ->
-            val selectedItem = menuItems[position]
-            if (!selectedItems.contains(selectedItem)) {
-                selectedItems.add(selectedItem)
-                orderAdapter.notifyDataSetChanged()
+        listView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            selectedItem = listView.getItemAtPosition(position) as Menucard.Orderlist
+        }
+
+        placeOrderB.setOnClickListener {
+            selectedItem?.let { item ->
+                val itemData = getItemData(item)
+
+                // Push the data to the Firebase Realtime Database
+                databaseRef.child("selectedItems").push().setValue(itemData)
             }
         }
 
-        // Remove item from order when clicked
-        orderListView.setOnItemClickListener { _, _, position, _ ->
-            val selectedItem = selectedItems[position]
-            selectedItems.remove(selectedItem)
-            orderAdapter.notifyDataSetChanged()
-        }
 
-        //Place order button click listener
-        placeOrderButton.setOnClickListener {
-            if (selectedItems.isNotEmpty()) {
-             //   Perform the necessary actions to place the order
-                        Toast.makeText(this, "Order placed!", Toast.LENGTH_SHORT).show()
-                selectedItems.clear()
-                orderAdapter.notifyDataSetChanged()
-            } else {
-                Toast.makeText(this, "Please select items to order", Toast.LENGTH_SHORT).show()
+
+
+
+            menuListView = findViewById(R.id.menuListView)
+            orderListView = findViewById(R.id.orderListView)
+            placeOrderButton = findViewById(R.id.placeOrderButton)
+
+            // Set up menu ListView
+            val menuAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, menuItems)
+            menuListView.adapter = menuAdapter
+
+            // Set up order ListView
+            orderAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, selectedItems)
+            orderListView.adapter = orderAdapter
+
+            // Add item to order when clicked
+            menuListView.setOnItemClickListener { _, _, position, _ ->
+                val selectedItem = menuItems[position]
+                if (!selectedItems.contains(selectedItem)) {
+                    selectedItems.add(selectedItem)
+                    orderAdapter.notifyDataSetChanged()
+                }
             }
+
+            // Remove item from order when clicked
+            orderListView.setOnItemClickListener { _, _, position, _ ->
+                val selectedItem = selectedItems[position]
+                selectedItems.remove(selectedItem)
+                orderAdapter.notifyDataSetChanged()
+            }
+
+            //Place order button click listener
+            placeOrderButton.setOnClickListener {
+                if (selectedItems.isNotEmpty()) {
+                    //   Perform the necessary actions to place the order
+                    Toast.makeText(this, "Order placed!", Toast.LENGTH_SHORT).show()
+                    selectedItems.clear()
+                    orderAdapter.notifyDataSetChanged()
+                } else {
+                    Toast.makeText(this, "Please select items to order", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        }
+        fun getItemData(selectedItem:Orderlist ): MutableMap<String, Any> {
+            // Create a map to store the data
+            val itemData: MutableMap<String, Any> = HashMap()
+
+            // Add the selected item's data to the map
+            itemData["order"] = selectedItem
+
+
+
+            return itemData
         }
 
+        data class Orderlist(val order: String)
     }
-}
+
 
